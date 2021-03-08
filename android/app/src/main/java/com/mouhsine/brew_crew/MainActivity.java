14a781957c0e8caa184dce1f78a04b3bcd9f06db
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-03-08 18:34:30 +0100

    init project

---
+++ android/app/src/main/java/com/mouhsine/brew_crew/MainActivity.java
@@ -0,0 +1,6 @@
+package com.mouhsine.brew_crew;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Container(
+      child: Text("this is a test"),
+    ),
+  ));
+}
+
