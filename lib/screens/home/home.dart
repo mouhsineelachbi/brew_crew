4932bcb2763d3bf0bab734ceec0e1f15a20ab1d4
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-03-11 18:12:42 +0100

    adding FlatButton with person icon to logout

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,8 +3,20 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Home'),
+    return Scaffold(
+      backgroundColor: Colors.red[50],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[500],
+        title: Text("Brew Crew"),
+        elevation: 0.0,
+        actions: [
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text("Logout"),
+            onPressed: (){},
+          )
+        ],
+      ),
     );
   }
 }
