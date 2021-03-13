7f55c918ee5da2328a7b55c63eddfbb3310a0bb0
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-03-13 21:00:05 +0100

    add stream to home page to get any changes in the documents

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,8 @@
 import 'package:brew_crew/services/auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_crew/services/database.dart';
 
 class Home extends StatelessWidget {
 
@@ -7,21 +10,25 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.red[50],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[500],
-        title: Text("Brew Crew"),
-        elevation: 0.0,
-        actions: [
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text("Logout"),
-            onPressed: () async {
-              await _auth.signOut();
-            },
-          )
-        ],
+    return StreamProvider<QuerySnapshot>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.red[50],
+        appBar: AppBar(
+          backgroundColor: Colors.brown[500],
+          title: Text("Brew Crew"),
+          elevation: 0.0,
+          actions: [
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text("Logout"),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            )
+          ],
+        ),
+        body: BewList(),
       ),
     );
   }
