01fdf13f19cd15040400acf256dff2b431b3a857
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-03-14 02:19:27 +0100

    created settings screen

---
+++ lib/screens/home/settings_form.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SettingsForm extends StatefulWidget {
+  @override
+  _SettingsFormState createState() => _SettingsFormState();
+}
+
+class _SettingsFormState extends State<SettingsForm> {
+
+  final _formKey = GlobalKey<FormState>();
+  final List<String> sugars = ['0', '1', '2', '3', '4'];
+
+  //  Form values
+  String _currentName;
+  String _currentSugars;
+  int _currentLength;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          Text(
+            'Update your brew settings',
+            style: TextStyle(fontSize: 18.0),
+          )
+        ],
+      ),
+    );
+  }
+}
