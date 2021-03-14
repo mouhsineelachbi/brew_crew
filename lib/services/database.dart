c3e5c019ed7cf7e40b9245f7f0651ec319d8f144
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-03-14 01:44:08 +0100

    extracting brew data from snapshots and push it to the stream provider

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/models/brew.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
@@ -17,8 +18,20 @@ class DatabaseService {
     });
   }
 
+  //  Brew list from snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return Brew(
+          name: doc.data['name'] ?? '',
+          sugars: doc.data['sugars']?? '',
+          strength: doc.data['strength'] ?? 0
+      );
+    }).toList();
+  }
+
   //  get brews(Documents) stream
-  Stream<QuerySnapshot> get brews {
-    return brewCollection.snapshots();
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots()
+    .map(_brewListFromSnapshot);
   }
 }
